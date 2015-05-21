d94afab8e8af7c147ef9d7f965017b45a5361df4
Dolan Murphy <admin@dmur.me>
2015-05-20 17:56:14 -0700

    Added database seed objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+SceneObject.create!([
+  {
+    id: 1,
+    name: 'Box 1',
+    kind: 'Cube', 
+    locked: false,
+    hidden: false,
+    position_x: 0.0,
+    position_y: 0.0,
+    position_z: 0.0,
+    scale_x: 1.0,
+    scale_y: 1.0,
+    scale_z: 1.0,
+    rotation_x: 0.0,
+    rotation_y: 0.0,
+    rotation_z: 0.0
+  },
+  {
+    id: 2,
+    name: 'Ball 1',
+    kind: 'Sphere',
+    locked: true,
+    hidden: false,
+    position_x: 1.5,
+    position_y: 2.0,
+    position_z: 0.0,
+    scale_x: 1.0,
+    scale_y: 1.0,
+    scale_z: 1.0,
+    rotation_x: 0.0,
+    rotation_y: 0.0,
+    rotation_z: 0.0
+  }
+])
